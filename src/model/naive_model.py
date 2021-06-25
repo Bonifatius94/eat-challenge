388d1fddb897b1a356808cb1d4509d5f574a562b
Marco Tröster <marco.troester@student.uni-augsburg.de>
2021-06-26 01:29:29 +0200

    named each model layer

--- src/model/naive_model.py
+++ src/model/naive_model.py
@@ -13,27 +13,27 @@ class NaiveEatModel(tf.keras.Model):
         dropout_rate = 0.4
 
         # first convolution layer with batch normalization and max pooling
-        self.nn_conv_1 = Conv2D(32, (5, 5), strides=1, padding='same')
-        self.nn_bnorm_1 = BatchNormalization()
-        self.act_1 = Activation('relu')
-        self.dropout_1 = Dropout(rate=dropout_rate)
-        self.maxpool_1 = MaxPooling2D()
+        self.nn_conv_1 = Conv2D(32, (5, 5), strides=1, padding='same', name='nn_conv_1')
+        self.nn_bnorm_1 = BatchNormalization(name='nn_bnorm_1')
+        self.act_1 = Activation('relu', name='act_1')
+        self.dropout_1 = Dropout(rate=dropout_rate, name='dropout_1')
+        self.maxpool_1 = MaxPooling2D(name='maxpool_1')
 
         # second convolution layer only with max pooling
-        self.nn_conv_2 = Conv2D(32, (3, 3), strides=1, padding='same')
-        #self.nn_bnorm_2 = BatchNormalization()
-        self.act_2 = Activation('relu')
-        self.dropout_2 = Dropout(rate=dropout_rate)
-        self.maxpool_2 = MaxPooling2D()
+        self.nn_conv_2 = Conv2D(32, (3, 3), strides=1, padding='same', name='nn_conv_2')
+        #self.nn_bnorm_2 = BatchNormalization(name='nn_bnorm_2')
+        self.act_2 = Activation('relu', name='act_2')
+        self.dropout_2 = Dropout(rate=dropout_rate, name='dropout_2')
+        self.maxpool_2 = MaxPooling2D(name='maxpool_2')
 
         # TODO: think of adding a 3rd Conv2D layer
         # TODO: think of increasing the amount of conv filters
         # TODO: think of add L2 loss regularization
 
         # output logits layer
-        self.flatten = Flatten()
-        self.nn_dense_3 = Dense(500, activation='relu')
-        self.nn_dense_out = Dense(num_classes, activation='softmax')
+        self.flatten = Flatten(name='flatten')
+        self.nn_dense_3 = Dense(500, activation='relu', name='nn_dense_3')
+        self.nn_dense_out = Dense(num_classes, activation='softmax', name='nn_dense_out')
 
 
     def call(self, inputs, training=False):
