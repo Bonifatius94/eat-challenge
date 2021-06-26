ccc8febf3002a6442680c05fdf33b2e46acff3d5
Marco Tröster <marco.troester@student.uni-augsburg.de>
2021-06-26 22:40:11 +0200

    - refactored main procedures - exported  configuration to json setting files - added abstraction layers using factories etc. - changed script call arguments

--- src/main.py
+++ src/main.py
@@ -1,61 +1,85 @@
 
-import sys
-from train import NaiveTrainingSession
-from dataset import write_tfrecord_datasets
+import sys, json, glob, os
+from train import TrainSessionFactory
+from dataset import DatasetFactory
+from eval import EvalSession
 
 
 def main():
 
-    # make sure there is a task specified as first script arg
+    # make sure the correct amount script args is provided
     if len(sys.argv) <= 1:
         raise ValueError('Invalid script arguments! Requires the task to be executed as first argument!')
+    if len(sys.argv) <= 2:
+        raise ValueError('Invalid script arguments! Requires the training type as second argument!')
 
     # parse the task from script args
     task = sys.argv[1]
+    train_type = sys.argv[2]
 
     # run the task
-    if task == 'preprocess_dataset': preprocess_dataset()
-    if task == 'naive_training': run_naive_training()
+    if task == 'preprocess_dataset': preprocess_dataset(train_type)
+    elif task == 'run_training': run_training(train_type)
+    elif task == 'eval_results': eval_results(train_type)
+    else: raise ValueError(f'Unknown main task { task }!')
     # TODO: add more tasks here ...
 
 
-def preprocess_dataset():
+def preprocess_dataset(train_type: str):
 
-    # define hyper-params required for preprocessing the dataset
-    params =  {
-        'seconds_per_sample': 4,
-        'sample_rate': 48000,
-        'num_map_threads': 3,
-    }
+    train_params = load_train_params(train_type)
 
-    # write a tfrecord cache file for each dataset (train / test)
-    write_tfrecord_datasets(params)
+    # create a preprocessed melspec dataset (TFRecord) from raw audio data
+    factory = DatasetFactory()
+    factory.create_cached_dataset(train_params)
 
 
-def run_naive_training():
+def run_training(train_type: str):
 
-    # define hyper-params required for creating the training session
-    params = {
-        # training hparams
-        'skip_training': False,
-        'num_epochs': 10,
-        'batch_size': 16, # batch size seems to be very volatile
+    train_params = load_train_params(train_type)
 
-        # dataset hparams
-        'num_train_samples': 945,
-        'num_test_samples': 469,
-        'seconds_per_sample': 4,
-        # 'sample_rate': 48000,
-        'sample_rate': 16000,
-        'num_map_threads': 3,
-        'num_classes': 7,
-        # 'melspec_shape': (128, 376, 1),
-        'melspec_shape': (128, 126, 1),
-    }
+    # create a training session from the loaded training params
+    train_sess_factory = TrainSessionFactory()
+    train_sess = train_sess_factory.create_train_session(train_type, train_params)
 
-    # load a dataset for testing purposes
-    session = NaiveTrainingSession(params)
-    session.run_training()
+    # run the training
+    train_sess.run_training()
+
+
+def eval_results(train_type: str):
+
+    train_params = load_train_params(train_type)
+
+    # load the best pre-trained model of the given training type
+    train_sess_factory = TrainSessionFactory()
+    train_sess = train_sess_factory.create_train_session(train_type, train_params)
+    train_sess.load_best_model()
+
+    # load the test dataset to be sampled on
+    factory = DatasetFactory()
+    dataset = factory.load_dataset(train_params['dataset_specifier'], train_params, train=False)
+    dataset = dataset.batch(train_params['batch_size'])
+
+    # make sure the eval results outdir exists
+    eval_outdir = './eval_results'
+    if not (os.path.exists(eval_outdir) and os.path.isdir(eval_outdir)):
+        os.makedirs(eval_outdir)
+
+    # perform the evaluation and write the results to csv file
+    session = EvalSession(dataset, train_sess.model)
+    session.export_results(os.path.join(eval_outdir, f'{ train_type }.csv'))
+
+
+def load_train_params(train_type: str):
+
+    # load the training parameters of the given training type
+    train_config_file = glob.glob(f'./config/*{ train_type }*.json')[0]
+    return load_config_params(train_config_file)
+
+
+def load_config_params(config_filepath: str):
+    with open(config_filepath, 'r') as file:
+        return json.load(file)
 
 
 if __name__ == '__main__':
