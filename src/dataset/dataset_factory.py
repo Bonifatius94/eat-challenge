ad7fc3decd5ee11c5387ab8e54b69097caf6cd47
Marco Tröster <marco@troester-gmbh.de>
2021-10-26 02:37:36 +0200

    using refactored approaches

--- src/dataset/dataset_factory.py
+++ src/dataset/dataset_factory.py
@@ -5,8 +5,11 @@ class DatasetFactory:
     def create_cached_dataset(self, dataset_specifier: str, params: dict):
 
         if dataset_specifier == 'tfrecord_melspec':
-            dataset.melspec.write_tfrecord_dataset(params, train=True)
-            dataset.melspec.write_tfrecord_dataset(params, train=False)
+            loader = dataset.melspec.CachedTfrecordMelspecDatasetLoader(params)
+            print('write training dataset')
+            loader.write_tfrecord_dataset(train=True)
+            print('write eval dataset')
+            loader.write_tfrecord_dataset(train=False)
         elif dataset_specifier == 'tfrecord_amplitude':
             dataset.amplitude.write_tfrecord_dataset(params, train=True)
             dataset.amplitude.write_tfrecord_dataset(params, train=False)
@@ -17,9 +20,14 @@ class DatasetFactory:
     def load_dataset(self, dataset_specifier: str, params: dict, train: bool):
 
         # load the dataset of the given type
-        if dataset_specifier == 'mapped_melspec': return dataset.melspec.load_mapped_dataset(params, train=train)
-        elif dataset_specifier == 'tfrecord_melspec': return dataset.melspec.load_tfrecord_dataset(params, train=train)
-        # elif dataset_specifier == 'np_ram_melspec': return dataset.melspec.load_cached_np_dataset(params, train=train)
-        if dataset_specifier == 'mapped_amplitude': return dataset.amplitude.load_mapped_dataset(params, train=train)
-        elif dataset_specifier == 'tfrecord_amplitude': return dataset.amplitude.load_tfrecord_dataset(params, train=train)
+        if dataset_specifier == 'mapped_melspec':
+            return dataset.melspec.MappedMelspecDatasetLoader(params).load_dataset(train=train)
+        elif dataset_specifier == 'tfrecord_melspec':
+            return dataset.melspec.CachedTfrecordMelspecDatasetLoader(params).load_dataset(train=train)
+        # elif dataset_specifier == 'np_ram_melspec':
+        #     return dataset.melspec.load_cached_np_dataset(params, train=train)
+        if dataset_specifier == 'mapped_amplitude':
+            return dataset.amplitude.load_mapped_dataset(params, train=train)
+        elif dataset_specifier == 'tfrecord_amplitude':
+            return dataset.amplitude.load_tfrecord_dataset(params, train=train)
         else: raise ValueError(f'Cannot load data for unknown dataset specifier { dataset_specifier }!')
--- src/dataset/melspec/__init__.py
+++ src/dataset/melspec/__init__.py
@@ -1,3 +1,5 @@
-from .mapped_dataset import load_dataset as load_mapped_dataset
+# from .mapped_dataset import load_dataset as load_mapped_dataset
 # from .cached_np_dataset import load_dataset as load_cached_np_dataset
-from .cached_tfrecord_dataset import load_dataset as load_tfrecord_dataset, write_tfrecord_dataset
+# from .cached_tfrecord_dataset import load_dataset as load_tfrecord_dataset, write_tfrecord_dataset
+from .mapped_dataset_loader import MappedMelspecDatasetLoader
+from .cached_tfrecord_dataset_loader import CachedTfrecordMelspecDatasetLoader
